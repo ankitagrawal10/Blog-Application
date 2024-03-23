d37080f9d7127b975068add84b7746574e0a3ad7
YourGitHubUsername <YourGitHubEmail>
2024-03-23 09:56:13 +0530

    resolve image issue

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,16 +4,19 @@ import { Link } from "react-router-dom";
 
 function PostCard({ $id, title, featuredImage }) {
   return (
-    <Link to={`/post/${$id}`}>
+    <Link to={`/post/${$id}`} className="block w-full h-full">
       <div className="w-full bg-gray-600 rounded-xl p-4 hover:bg-cyan-500 transform transition duration-300 hover:scale-105">
-        <div className="w-full justify-center mb-4">
+        <div className="relative w-full bg-gray-200 rounded-xl overflow-hidden border border-gray-300">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
-            className="rounded-xl"
+            className="w-full h-auto object-cover"
+            style={{ aspectRatio: "9/9" }} // Adjust aspect ratio as needed
           />
         </div>
-        <h2 className="text-xl font-bold text-white">{title}</h2>
+        <div className="bg-gray-600 text-white text-center p-4 ">
+          <h2 className="text-xl font-bold">{title}</h2>
+        </div>
       </div>
     </Link>
   );
